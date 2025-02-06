7de36cccecf7251dff41629839985065999e26c2
ivatest-3d <eclark@blur.pm>
2025-02-06 16:30:01 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,475 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-giri",
+      "name": "Giriş",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.26851219261452286,
+        "pitch": -0.043101944889039956,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.49880224955549934,
+          "pitch": -0.43366606780479344,
+          "rotation": 0,
+          "target": "5-penthouse-teras"
+        },
+        {
+          "yaw": -0.06672631624489966,
+          "pitch": 0.19052602012014752,
+          "rotation": 0,
+          "target": "1-a-b-blok-girii"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-a-b-blok-girii",
+      "name": "A-B Blok Girişi",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.24434703499350263,
+        "pitch": -0.026237278143103282,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7956158507017115,
+          "pitch": -0.14847348898167567,
+          "rotation": 0,
+          "target": "7-bahe"
+        },
+        {
+          "yaw": 1.2586032541730656,
+          "pitch": 0.022365319615104795,
+          "rotation": 0,
+          "target": "8-bahe"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-villa-girii",
+      "name": "Villa Girişi",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.8176213423536716,
+        "pitch": 0.06183952588963315,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.004389473059978499,
+          "pitch": -0.16225048856859559,
+          "rotation": 0,
+          "target": "4-villa-teras"
+        },
+        {
+          "yaw": 1.7701339466539352,
+          "pitch": -0.0245679246590349,
+          "rotation": 0,
+          "target": "8-bahe"
+        },
+        {
+          "yaw": 2.2799223262617208,
+          "pitch": 0.00005025601437225191,
+          "rotation": 0,
+          "target": "9-gnelenme-teras"
+        },
+        {
+          "yaw": -2.9967042059404587,
+          "pitch": 0.043084782600802285,
+          "rotation": 0,
+          "target": "7-bahe"
+        },
+        {
+          "yaw": -3.0706982975526813,
+          "pitch": -0.4300217906820336,
+          "rotation": 0,
+          "target": "3-teras"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-teras",
+      "name": "Teras",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.5222554951664797,
+        "pitch": 0.05809292145819356,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5856475609106617,
+          "pitch": 0.3709240704158461,
+          "rotation": 0,
+          "target": "2-villa-girii"
+        },
+        {
+          "yaw": 0.9613517696708378,
+          "pitch": -0.009627597627012818,
+          "rotation": 0,
+          "target": "6-genel-grnm"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-villa-teras",
+      "name": "Villa Terası",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8924478153300157,
+          "pitch": -0.39049396972832895,
+          "rotation": 0,
+          "target": "6-genel-grnm"
+        },
+        {
+          "yaw": -1.3633380994290647,
+          "pitch": -0.09062466759540477,
+          "rotation": 0,
+          "target": "2-villa-girii"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-penthouse-teras",
+      "name": "Penthouse Terası",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4615737254559562,
+          "pitch": -0.23043986288338658,
+          "rotation": 0,
+          "target": "5-penthouse-teras"
+        },
+        {
+          "yaw": -0.8430682127151119,
+          "pitch": 0.5367784203889663,
+          "rotation": 0,
+          "target": "7-bahe"
+        },
+        {
+          "yaw": -0.4142738310512897,
+          "pitch": 0.706249203374778,
+          "rotation": 0,
+          "target": "2-villa-girii"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-genel-grnm",
+      "name": "Genel Görünüm",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.23763329046384207,
+        "pitch": 0.17240777955615627,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.11056780522218368,
+          "pitch": -0.21721121862136883,
+          "rotation": 0,
+          "target": "5-penthouse-teras"
+        },
+        {
+          "yaw": -0.09561338447293721,
+          "pitch": 0.11266544906527187,
+          "rotation": 0,
+          "target": "8-bahe"
+        },
+        {
+          "yaw": 0.5462957348810118,
+          "pitch": 0.25973202758002145,
+          "rotation": 0,
+          "target": "4-villa-teras"
+        },
+        {
+          "yaw": 0.2376332677381825,
+          "pitch": -0.09421983790003097,
+          "rotation": 0,
+          "target": "3-teras"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-bahe",
+      "name": "Bahçe",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.5115253405970535,
+        "pitch": 0.0056354119324666385,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.22068059223111725,
+          "pitch": -0.06175722215946777,
+          "rotation": 0,
+          "target": "2-villa-girii"
+        },
+        {
+          "yaw": 0.9587725335583634,
+          "pitch": 0.009085482691642,
+          "rotation": 0,
+          "target": "9-gnelenme-teras"
+        },
+        {
+          "yaw": -2.761098148017778,
+          "pitch": -0.4476281800490227,
+          "rotation": 0,
+          "target": "1-a-b-blok-girii"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-bahe",
+      "name": "Bahçe",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -2.226159381662839,
+        "pitch": -0.000203125524068426,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.623522076731195,
+          "pitch": 0.01619173061461865,
+          "rotation": 0,
+          "target": "7-bahe"
+        },
+        {
+          "yaw": -2.3542479654249,
+          "pitch": 0.021818875059448573,
+          "rotation": 0,
+          "target": "2-villa-girii"
+        },
+        {
+          "yaw": 1.6745618425233637,
+          "pitch": -0.3240175859313279,
+          "rotation": 0,
+          "target": "1-a-b-blok-girii"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-gnelenme-teras",
+      "name": "Güneşlenme Terası",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.696991509613266,
+          "pitch": -0.04594021719201358,
+          "rotation": 0,
+          "target": "2-villa-girii"
+        },
+        {
+          "yaw": -2.0459807366243012,
+          "pitch": 0.015905336806390835,
+          "rotation": 0,
+          "target": "7-bahe"
+        },
+        {
+          "yaw": 0.4976459670113744,
+          "pitch": -0.5761315746602058,
+          "rotation": 0,
+          "target": "6-genel-grnm"
+        },
+        {
+          "yaw": 1.2059092246552332,
+          "pitch": -0.07586222880568272,
+          "rotation": 0,
+          "target": "8-bahe"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Balmumcu Dış Mekan Sanal Tur",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
